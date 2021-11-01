795cd1711e30b632120211cba2d00d23a9e71bea
tymaestro <tymaestro22@gmail.com>
2021-11-01 10:21:49 +0000

    set up structure of game function

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculatecorrectAnswer() {
+
+}
+
+function incorrectScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+    
+}
+
+function displayMultiplyQuestion() {
+    
+}
